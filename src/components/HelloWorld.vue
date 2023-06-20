bcdd416af71c577396f6b9f50db3e46ea78b6e91
marcelorang <mclowebdev@gmail.com>
2023-06-20 03:02:46 -0300

    adicionando curriculo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -17,53 +17,108 @@
 
                 <p style="color: black">mclowebdev@gmail.com</p>
                 <br />
-                <v-row>
+                <v-row align="center" align-content="space-be">
                   <v-col>
-                    <div>
-                      <a href="https://github.com/marcelorang">
-                        <svg width="48" height="48" enable-background="new 0 0 512 512" id="Layer_1" version="1.1"
-                          viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"
-                          xmlns:xlink="http://www.w3.org/1999/xlink">
+
+                    <a href="https://github.com/marcelorang">
+                      <svg width="48" height="48" enable-background="new 0 0 512 512" id="Layer_1" version="1.1"
+                        viewBox="0 0 512 512" xml:space="preserve" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink">
+                        <g>
+                          <circle cx="256" cy="256" fill="#333333" r="256" />
                           <g>
-                            <circle cx="256" cy="256" fill="#333333" r="256" />
-                            <g>
-                              <path
-                                d="M256,93.9c-89.5,0-162.1,72.6-162.1,162.1c0,70.5,45,130.4,107.8,152.8c0.3,0.1,1.4,0.5,1.7,0.6    c0.9,0.3,1.9,0.5,2.9,0.5c5.3,0,9.5-4.3,9.5-9.5c0-0.3,0-0.5,0-0.8l0,0c0-8.6,0-19.5,0-28.2c-10.3,2.1-25.9,4.1-34.4,0    c-11-5.3-16.6-12.1-21.9-25.5c-6.6-16.3-21.8-20.8-22.4-23.6c-0.6-2.9,16.1-7.2,24.7,2.7c8.6,9.9,17.3,29.7,35.8,27.8    c9.1-0.9,15-2.4,18.7-3.5c0.7-6.4,2.8-14.3,8.1-19.9c-43.5-7.2-72.5-30.6-72.5-76.5c0-20.9,6-37.1,16.6-49.2    c-1.8-10-4.8-33.2,3.2-41.3c0,0,11.3-7,43.7,15.8c12.1-2.8,25.5-4.2,39.8-4.2l0,0c0.3,0,0.5,0,0.8,0c0.3,0,0.5,0,0.8,0l0,0    c14.4,0.1,27.8,1.5,39.8,4.2c32.4-22.8,43.7-15.8,43.7-15.8c8,8.2,5,31.4,3.2,41.3c10.6,12.2,16.6,28.4,16.6,49.2    c0,45.9-28.9,69.3-72.5,76.5c8.3,8.7,8.6,22.8,8.6,28.6c0,5.5,0,42.3,0,42.5c0,5.3,4.3,9.5,9.5,9.5c0.8,0,1.5-0.1,2.3-0.3    c0.2,0,0.8-0.2,1-0.3c63.5-22,109.1-82.3,109.1-153.3C418.1,166.4,345.5,93.9,256,93.9z"
-                                fill="#FFFFFF" />
-                            </g>
+                            <path
+                              d="M256,93.9c-89.5,0-162.1,72.6-162.1,162.1c0,70.5,45,130.4,107.8,152.8c0.3,0.1,1.4,0.5,1.7,0.6    c0.9,0.3,1.9,0.5,2.9,0.5c5.3,0,9.5-4.3,9.5-9.5c0-0.3,0-0.5,0-0.8l0,0c0-8.6,0-19.5,0-28.2c-10.3,2.1-25.9,4.1-34.4,0    c-11-5.3-16.6-12.1-21.9-25.5c-6.6-16.3-21.8-20.8-22.4-23.6c-0.6-2.9,16.1-7.2,24.7,2.7c8.6,9.9,17.3,29.7,35.8,27.8    c9.1-0.9,15-2.4,18.7-3.5c0.7-6.4,2.8-14.3,8.1-19.9c-43.5-7.2-72.5-30.6-72.5-76.5c0-20.9,6-37.1,16.6-49.2    c-1.8-10-4.8-33.2,3.2-41.3c0,0,11.3-7,43.7,15.8c12.1-2.8,25.5-4.2,39.8-4.2l0,0c0.3,0,0.5,0,0.8,0c0.3,0,0.5,0,0.8,0l0,0    c14.4,0.1,27.8,1.5,39.8,4.2c32.4-22.8,43.7-15.8,43.7-15.8c8,8.2,5,31.4,3.2,41.3c10.6,12.2,16.6,28.4,16.6,49.2    c0,45.9-28.9,69.3-72.5,76.5c8.3,8.7,8.6,22.8,8.6,28.6c0,5.5,0,42.3,0,42.5c0,5.3,4.3,9.5,9.5,9.5c0.8,0,1.5-0.1,2.3-0.3    c0.2,0,0.8-0.2,1-0.3c63.5-22,109.1-82.3,109.1-153.3C418.1,166.4,345.5,93.9,256,93.9z"
+                              fill="#FFFFFF" />
                           </g>
-                        </svg>
-                        <p>GitHub</p>
-                      </a>
-                    </div>
+                        </g>
+                      </svg>
+                      <p>GitHub</p>
+                    </a>
+
                   </v-col>
+
                   <v-col>
-                    <div>
-                      <a href="https://www.linkedin.com/in/marcelo-rangel-622089221/">
-                        <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <path
-                            d="M0 24C0 10.7452 10.7452 0 24 0C37.2548 0 48 10.7452 48 24C48 37.2548 37.2548 48 24 48C10.7452 48 0 37.2548 0 24Z"
-                            fill="#0077B5" />
-                          <path fill-rule="evenodd" clip-rule="evenodd"
-                            d="M17.3188 14.8227C17.3188 16.3918 16.1377 17.6473 14.2412 17.6473H14.2064C12.3805 17.6473 11.2 16.3918 11.2 14.8227C11.2 13.2204 12.4164 12 14.277 12C16.1377 12 17.2835 13.2204 17.3188 14.8227ZM16.9605 19.8778V36.2196H11.5216V19.8778H16.9605ZM36.5752 36.2196L36.5754 26.8497C36.5754 21.8303 33.8922 19.4941 30.3131 19.4941C27.4254 19.4941 26.1325 21.0802 25.4107 22.1929V19.8783H19.9711C20.0428 21.4117 19.9711 36.22 19.9711 36.22H25.4107V27.0934C25.4107 26.605 25.446 26.1178 25.5898 25.7681C25.9829 24.7924 26.8779 23.7822 28.3805 23.7822C30.3494 23.7822 31.1365 25.2807 31.1365 27.4767V36.2196H36.5752Z"
-                            fill="white" />
-                        </svg>
-                        <p>LinkedIn</p>
-                      </a>
-                    </div>
+
+                    <a href="https://www.linkedin.com/in/marcelo-rangel-622089221/">
+                      <svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                          d="M0 24C0 10.7452 10.7452 0 24 0C37.2548 0 48 10.7452 48 24C48 37.2548 37.2548 48 24 48C10.7452 48 0 37.2548 0 24Z"
+                          fill="#0077B5" />
+                        <path fill-rule="evenodd" clip-rule="evenodd"
+                          d="M17.3188 14.8227C17.3188 16.3918 16.1377 17.6473 14.2412 17.6473H14.2064C12.3805 17.6473 11.2 16.3918 11.2 14.8227C11.2 13.2204 12.4164 12 14.277 12C16.1377 12 17.2835 13.2204 17.3188 14.8227ZM16.9605 19.8778V36.2196H11.5216V19.8778H16.9605ZM36.5752 36.2196L36.5754 26.8497C36.5754 21.8303 33.8922 19.4941 30.3131 19.4941C27.4254 19.4941 26.1325 21.0802 25.4107 22.1929V19.8783H19.9711C20.0428 21.4117 19.9711 36.22 19.9711 36.22H25.4107V27.0934C25.4107 26.605 25.446 26.1178 25.5898 25.7681C25.9829 24.7924 26.8779 23.7822 28.3805 23.7822C30.3494 23.7822 31.1365 25.2807 31.1365 27.4767V36.2196H36.5752Z"
+                          fill="white" />
+                      </svg>
+                      <p>LinkedIn</p>
+                    </a>
+
                   </v-col>
+
+                  <v-col>
+
+                    <a href="../assets/CurriculoDev.pdf" download="">
+                      <svg height="48px" width="48px" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg"
+                        xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 303.188 303.188" xml:space="preserve">
+                        <g>
+                          <polygon style="fill:#E8E8E8;"
+                            points="219.821,0 32.842,0 32.842,303.188 270.346,303.188 270.346,50.525 	" />
+                          <path style="fill:#FB3449;"
+                            d="M230.013,149.935c-3.643-6.493-16.231-8.533-22.006-9.451c-4.552-0.724-9.199-0.94-13.803-0.936
+		c-3.615-0.024-7.177,0.154-10.693,0.354c-1.296,0.087-2.579,0.199-3.861,0.31c-1.314-1.36-2.584-2.765-3.813-4.202
+		c-7.82-9.257-14.134-19.755-19.279-30.664c1.366-5.271,2.459-10.772,3.119-16.485c1.205-10.427,1.619-22.31-2.288-32.251
+		c-1.349-3.431-4.946-7.608-9.096-5.528c-4.771,2.392-6.113,9.169-6.502,13.973c-0.313,3.883-0.094,7.776,0.558,11.594
+		c0.664,3.844,1.733,7.494,2.897,11.139c1.086,3.342,2.283,6.658,3.588,9.943c-0.828,2.586-1.707,5.127-2.63,7.603
+		c-2.152,5.643-4.479,11.004-6.717,16.161c-1.18,2.557-2.335,5.06-3.465,7.507c-3.576,7.855-7.458,15.566-11.815,23.02
+		c-10.163,3.585-19.283,7.741-26.857,12.625c-4.063,2.625-7.652,5.476-10.641,8.603c-2.822,2.952-5.69,6.783-5.941,11.024
+		c-0.141,2.394,0.807,4.717,2.768,6.137c2.697,2.015,6.271,1.881,9.4,1.225c10.25-2.15,18.121-10.961,24.824-18.387
+		c4.617-5.115,9.872-11.61,15.369-19.465c0.012-0.018,0.024-0.036,0.037-0.054c9.428-2.923,19.689-5.391,30.579-7.205
+		c4.975-0.825,10.082-1.5,15.291-1.974c3.663,3.431,7.621,6.555,11.939,9.164c3.363,2.069,6.94,3.816,10.684,5.119
+		c3.786,1.237,7.595,2.247,11.528,2.886c1.986,0.284,4.017,0.413,6.092,0.335c4.631-0.175,11.278-1.951,11.714-7.57
+		C231.127,152.765,230.756,151.257,230.013,149.935z M119.144,160.245c-2.169,3.36-4.261,6.382-6.232,9.041
+		c-4.827,6.568-10.34,14.369-18.322,17.286c-1.516,0.554-3.512,1.126-5.616,1.002c-1.874-0.11-3.722-0.937-3.637-3.065
+		c0.042-1.114,0.587-2.535,1.423-3.931c0.915-1.531,2.048-2.935,3.275-4.226c2.629-2.762,5.953-5.439,9.777-7.918
+		c5.865-3.805,12.867-7.23,20.672-10.286C120.035,158.858,119.587,159.564,119.144,160.245z M146.366,75.985
+		c-0.602-3.514-0.693-7.077-0.323-10.503c0.184-1.713,0.533-3.385,1.038-4.952c0.428-1.33,1.352-4.576,2.826-4.993
+		c2.43-0.688,3.177,4.529,3.452,6.005c1.566,8.396,0.186,17.733-1.693,25.969c-0.299,1.31-0.632,2.599-0.973,3.883
+		c-0.582-1.601-1.137-3.207-1.648-4.821C147.945,83.048,146.939,79.482,146.366,75.985z M163.049,142.265
+		c-9.13,1.48-17.815,3.419-25.979,5.708c0.983-0.275,5.475-8.788,6.477-10.555c4.721-8.315,8.583-17.042,11.358-26.197
+		c4.9,9.691,10.847,18.962,18.153,27.214c0.673,0.749,1.357,1.489,2.053,2.22C171.017,141.096,166.988,141.633,163.049,142.265z
+		 M224.793,153.959c-0.334,1.805-4.189,2.837-5.988,3.121c-5.316,0.836-10.94,0.167-16.028-1.542
+		c-3.491-1.172-6.858-2.768-10.057-4.688c-3.18-1.921-6.155-4.181-8.936-6.673c3.429-0.206,6.9-0.341,10.388-0.275
+		c3.488,0.035,7.003,0.211,10.475,0.664c6.511,0.726,13.807,2.961,18.932,7.186C224.588,152.585,224.91,153.321,224.793,153.959z" />
+                          <polygon style="fill:#FB3449;" points="227.64,25.263 32.842,25.263 32.842,0 219.821,0 	" />
+                          <g>
+                            <path style="fill:#A4A9AD;" d="M126.841,241.152c0,5.361-1.58,9.501-4.742,12.421c-3.162,2.921-7.652,4.381-13.472,4.381h-3.643
+			v15.917H92.022v-47.979h16.606c6.06,0,10.611,1.324,13.652,3.971C125.321,232.51,126.841,236.273,126.841,241.152z
+			 M104.985,247.387h2.363c1.947,0,3.495-0.546,4.644-1.641c1.149-1.094,1.723-2.604,1.723-4.529c0-3.238-1.794-4.857-5.382-4.857
+			h-3.348C104.985,236.36,104.985,247.387,104.985,247.387z" />
+                            <path style="fill:#A4A9AD;" d="M175.215,248.864c0,8.007-2.205,14.177-6.613,18.509s-10.606,6.498-18.591,6.498h-15.523v-47.979
+			h16.606c7.701,0,13.646,1.969,17.836,5.907C173.119,235.737,175.215,241.426,175.215,248.864z M161.76,249.324
+			c0-4.398-0.87-7.657-2.609-9.78c-1.739-2.122-4.381-3.183-7.926-3.183h-3.773v26.877h2.888c3.939,0,6.826-1.143,8.664-3.43
+			C160.841,257.523,161.76,254.028,161.76,249.324z" />
+                            <path style="fill:#A4A9AD;" d="M196.579,273.871h-12.766v-47.979h28.355v10.403h-15.589v9.156h14.374v10.403h-14.374
+			L196.579,273.871L196.579,273.871z" />
+                          </g>
+                          <polygon style="fill:#D1D3D3;" points="219.821,50.525 270.346,50.525 219.821,0 	" />
+                        </g>
+                      </svg>
+                      <p>Curriculo</p>
+                    </a>
+
+                  </v-col>
+
+
+
+
+
+
                 </v-row>
                 <br />
-                <!-- <h5>
-                Comprometido em adquirir conhecimentos e enfrentar desafios no
-                desenvolvimento de softwares. Buscando oportunidades para
-                aplicar sua paixão pela tecnologia e construir soluções
-                inovadoras. Determinado a alcançar objetivos e evoluir na
-                carreira.
-              </h5> -->
               </div>
             </v-card>
           </v-col>
+          <v-col></v-col>
+
 
           <v-col cols="12" lg="9" sm="6">
             <div>
@@ -104,44 +159,42 @@
               </v-row>
             </div>
           </v-col>
-          <v-col>
-          </v-col>
+          <v-col></v-col>
 
           <v-col cols="12" lg="6" sm="6">
-
-
             <v-sheet class="text-right mr-4">
-              Tecnologias: Vue, Ionic, Android Stúdio .NET C# <br />
+              <h3>
+                Tecnologias:
+              </h3>
+              Vue, Ionic, Android Stúdio .NET C# <br />
               Aplicativo Mobile.<br />
-              Colaboração FrontEnd
+              Colaboração como desenvolvedor FrontEnd.
             </v-sheet>
           </v-col>
+
           <v-col cols="12" lg="6" sm="6">
             <v-sheet class="text-left ml-4">
-              Tecnologias: Vue, Vuetify, C#, .NET. <br />
-              Dashboard Admin para clínica Médica, Odontologicas, Etc. Com
-              pronto sistema de agendamento de Consultas, <br />Cadastros de
-              Paciêntes, Colaboradores e médicos. <br />
-              Colaboração FrontEnd.<br />
+              <h3>
+                Tecnologias:
+              </h3> Vue, Vuetify, C#, .NET. <br />
+              Dashboard administrativo para clínicas médicas, odontologicas, etc. Com
+              pronto sistema de agendamento de consultas, <br />cadastros de
+              paciêntes, colaboradores e médicos. <br />
+              Colaboração como desenvolvedor frontEnd.<br />
               <p><a href="https://www.medzip.com.br">www.medzip.com.br</a></p>
             </v-sheet>
           </v-col>
-
         </v-row>
-
-
-
-
 
         <br />
         <v-card>
-          <div class="text-start pa-4" style="font-size: 20px">
-            Formado no curso Superior em Desenvolvimento Web pela faculdade
+          <div class="text-center pa-4" style="font-size: 20px">
+            Formado no curso Superior em Desenvolvimento Web pela Faculdade
             Anhanguera,<br />
             Começei no mundo da programação em agosto de 2021, quando estava
             parando de trabalhar como músico profissional por mais de quinze
             anos.<br />
-            Em meio a crise da Pandemia, vi que precisava migrar de Carreira e
+            Em meio a crise da pandemia, vi que precisava migrar de carreira e
             foi onde me encontrei na área de TI. <br />
             Comprometido em adquirir conhecimentos e enfrentar desafios no
             desenvolvimento de softwares.<br />
@@ -151,31 +204,20 @@
           </div>
         </v-card>
 
-
-
-
-
-
         <div id="scrolltoContact">
           <v-footer>
-
-
-
-
-
             <div class=" bg-blue-grey-lighten-5 text-center w-100">
               {{ new Date().getFullYear() }} — <strong>Marcelo Rangel</strong>
             </div>
           </v-footer>
         </div>
-
-
       </v-responsive>
     </v-container>
   </v-sheet>
 </template>
 
-<script>
+<script >
+
 import img1 from '../assets/images/medzip1.png'
 import img2 from '../assets/images/medzip2.png'
 import img3 from '../assets/images/medzip3.png'
@@ -186,11 +228,16 @@ import img7 from '../assets/images/telaPhone13.jpg'
 import img8 from '../assets/images/telaPhone14.jpg'
 import img9 from '../assets/images/telaPhone15.jpg'
 import img10 from '../assets/images/telaPhone16.jpg'
-
+import pdfMake from "pdfmake/build/pdfmake";
+import pdfFonts from "pdfmake/build/vfs_fonts";
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
+import curriculo from "../assets/CurriculoDev.pdf"
 
 export default {
   data() {
     return {
+      curriculum: [curriculo],
+      animals: ['Column 1', 'Column2', 'Column 3'],
       images: [img1, img2, img3, img4, img5],
       images2: [img6, img7, img8, img9, img10],
       tab: null,
@@ -210,6 +257,9 @@ export default {
     scrolltoContact() {
       const element = document.getElementById("scrolltoContact");
       element.scrollIntoView({ behavior: "smooth" });
+    },
+    downloadPdf() {
+      pdfMake.createPdf(docDefinition).download('');
     },
   },
 };
@@ -270,3 +320,22 @@ export default {
   right: 10px;
 }
 </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
